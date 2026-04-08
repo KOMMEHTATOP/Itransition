1927175953b16aaeed2474cfb6882af8b6152c7e
KOMMEHTATOP <bahtiyarbasharov@gmail.com>
2026-04-08 13:56:35 +0500

    Absolute paths have been removed

--- DATA_Task1_parseBook/Program.cs
+++ DATA_Task1_parseBook/Program.cs
@@ -1,17 +1,16 @@
-﻿using DATA_Task1_parseBook;
+﻿namespace DATA_Task1_parseBook;
 
-class Program 
+class Program
 {
     static void Main(string[] args)
     {
         var parser = new Parser();
-        var result= parser.ParseBook(@"C:\Users\basha\RiderProjects\Itransition\DATA_Task1_parseBook\task1_d.json");
-        
-        var db = new Database("Host=localhost;Database=books_db;Username=postgres;Password=123;");
-        
+        var result = parser.ParseBook(args[0]);
+
+        var db = new Database(args[1]);
+
         db.CreateTable();
         db.InsertBook(result);
         db.CreateSummary();
     }
 }
-
