3468ded97cd1a7178a9823c03dc079dac9ee7f42
KOMMEHTATOP <bahtiyarbasharov@gmail.com>
2026-04-23 16:53:08 +0500

    Fix package-lock and add db migrations on startup

--- Task6_CollabDraw/CollabDraw.Server/Program.cs
+++ Task6_CollabDraw/CollabDraw.Server/Program.cs
@@ -36,4 +36,10 @@ app.MapControllers();
 
 app.MapHub<WhiteboardHub>("/hub/whiteboard");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
