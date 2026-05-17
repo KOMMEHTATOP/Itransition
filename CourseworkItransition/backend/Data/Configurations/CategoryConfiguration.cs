834754891df15a3e29545286b908e5caa03ae455
KOMMEHTATOP <bahtiyarbasharov@gmail.com>
2026-05-17 22:33:26 +0500

    начал вынес контекста.

---
+++ CourseworkItransition/backend/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using InventoryApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryApi.Data.Configurations;
+
+public class CategoryConfiguration: IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasData(
+            new Category { Id = 1, Name = "Equipment" },
+            new Category { Id = 2, Name = "Furniture" },
+            new Category { Id = 3, Name = "Book" },
+            new Category { Id = 4, Name = "Other" }
+        );
+    }
+}
